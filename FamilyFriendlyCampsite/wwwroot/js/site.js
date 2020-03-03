d83dccf66b76e9e907b3a8c4f90f958ecc2ff6ef
asiobhan@Engineering.ukho.gov.uk <asiobhan@Engineering.ukho.gov.uk>
2020-03-03 13:45:49 +0000

    search bar added, images saved to file for use on site

--- FamilyFriendlyCampsite/wwwroot/js/site.js
+++ FamilyFriendlyCampsite/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+var defaultText = "Search...";
+var searchBox = document.getElementById("search");
+//default text after load 
+searchBox.value = defaultText;
+//on focus behaviour 
+searchBox.onfocus = function () { if (this.value == defaultText) { this.value = ''; } }
+//on blur behaviour 
+searchBox.onblur = function () { if (this.value == "") {this.value = defaultText; } }
